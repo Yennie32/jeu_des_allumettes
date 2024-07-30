7e74cccbf143cd1b6efddf86dbd38791742b088a
Your Name <you@example.com>
2024-07-30 14:39:01 +0200

    ajout tas d'allum positif + remise du tas avant prise

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Demander a l'utilisateur combien d'allumettes il souhaite retirer tant qu’il y a des allumettes dans le tas. Pour rappel, on a 50 allumettes au départ.
 
-let nombre_allumettes = 10
+let nombre_allumettes = 12
+
+
+function fourchette_allumettes(){
+   if(nombre_allumettes >= 0){
+      return true
+   } else {
+      return false
+   }
+}
 
 function tas_d_allumettes (retrait_allumettes) {
     retrait_allumettes = parseInt(prompt("Combien d'allumettes prends-tu ?"))
@@ -10,9 +19,15 @@ function tas_d_allumettes (retrait_allumettes) {
      } else {
         nombre_allumettes = nombre_allumettes - retrait_allumettes
         alert(`Il reste ${nombre_allumettes} allumettes`) 
-     } if (nombre_allumettes == 0){
-        return alert("Il n'y a plus d'allumettes")}
+     } if (nombre_allumettes < 0){
+      nombre_allumettes = nombre_allumettes + retrait_allumettes
+      alert(`Tu as pris trop d'allumettes. Il reste ${nombre_allumettes} allumettes`) 
+      return false
+     } if (nombre_allumettes === 0){
+        return alert("Bravo ! Il n'y a plus d'allumettes")}
      tas_d_allumettes()
+     fourchette_allumettes()
+     return true
     } 
     }
     
