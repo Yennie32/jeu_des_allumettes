c1fd6aae2db02ceac466ada08e29b8962c9b633d
Your Name <you@example.com>
2024-08-13 13:06:33 +0200

    amélioration de focntions du jeu

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 // Demander a l'utilisateur combien d'allumettes il souhaite retirer tant qu’il y a des allumettes dans le tas. Pour rappel, on a 50 allumettes au départ.
 
-let nombre_allumettes = 12
+let nombre_allumettes = 5; // initialisation du nombre d'allumettes de base
 
+function retrait_allumettes(){ // prompt + valeur du retrait d'allumettes
+   let nombre_retrait = parseInt(prompt("Combien d'allumettes prends-tu ?"));
+   return nombre_retrait; // retour de la valeur de retrait d'allumettes
+}
 
-function fourchette_allumettes(){
-   if(nombre_allumettes >= 0){
-      return true
-   } else {
-      return false
+function regles_allumettes(retrait){ // regles de retrait du nombre d'allumettes
+   if (retrait < 1 || retrait > 6){ // condition si retrait dépasse range de 1 à 6
+      alert("Tu peux retirer entre 1 et 6 allumettes");
+      return false; // booléen de validation de condition
+   } else if (retrait > nombre_allumettes) { // condition si retrait depasse le nombre d'allumettes restant
+      alert(`Il n'y a que ${nombre_allumettes} allumettes restantes. Tu ne peux pas en retirer ${retrait}.`);
+      return false; // booléen de validation de condition
+   }
+   return true;
+}
+
+function tas_d_allumettes(allumettes_retirees) { // décrémentation du nombre d'allumettes
+   nombre_allumettes -= allumettes_retirees;
+   alert(`Il reste ${nombre_allumettes} allumettes`);
+} 
+
+function tour_de_jeu() {
+   while (nombre_allumettes > 0){ // tant que le nombre d'allumettes est strictement sup à 0
+      let allumettes_retirees = retrait_allumettes(); // appel de la fonction retrait allumettes et mise en variable
+      if (regles_allumettes(allumettes_retirees)){ // vérifie que les règles sont respectées puisque que fonction booléenne
+         tas_d_allumettes(allumettes_retirees); // si retour de la fonction regles_allumettes true => éxecution du code tas_d'allumettes 
+      }
+   } 
+   if (nombre_allumettes === 0){ // condition de victoire
+      alert("Bravo, tu as pris la dernière allumette !");
    }
 }
 
-function tas_d_allumettes (retrait_allumettes) {
-    retrait_allumettes = parseInt(prompt("Combien d'allumettes prends-tu ?"))
-    while (nombre_allumettes > 0) {
-     if (retrait_allumettes < 1 || retrait_allumettes > 6){
-        alert("Tu peux retirer entre 1 et 6 allumettes")
-     } else {
-        nombre_allumettes = nombre_allumettes - retrait_allumettes
-        alert(`Il reste ${nombre_allumettes} allumettes`) 
-     } if (nombre_allumettes < 0){
-      nombre_allumettes = nombre_allumettes + retrait_allumettes
-      alert(`Tu as pris trop d'allumettes. Il reste ${nombre_allumettes} allumettes`) 
-      return false
-     } if (nombre_allumettes === 0){
-        return alert("Bravo ! Il n'y a plus d'allumettes")}
-     tas_d_allumettes()
-     fourchette_allumettes()
-     return true
-    } 
-    }
-    
-    
-tas_d_allumettes()
+tour_de_jeu();
 
